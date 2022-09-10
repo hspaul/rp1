d50b914a1ee5815977c6b3f4cad01ebe74cb2ecd
HeeSeok yea <hsyea@naver.com>
2022-09-10 22:00:36 +0900

    init commit

---
+++ src/net/test/first/MyClass1.java
@@ -0,0 +1,13 @@
+package net.test.first;
+
+//c:\mydata\jsp_ws1\TestJavaProject1
+// /c/mydata/jsp_ws1/TestJavaProject1
+
+public class MyClass1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello");
+	}
+
+}
